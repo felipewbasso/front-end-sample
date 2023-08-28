c2af3af7adca2536dfa396a9ae8de7d3e353dc48
Felipe Basso <felipe.basso@techer.com.br>
2023-08-28 11:29:09 -0300

    material modules with common providers

---
+++ src/app/shared/modules/providers/material-modules.provider.ts
@@ -0,0 +1,9 @@
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+
+export const materialModulesProviders = [
+    MatTooltipModule,
+    MatDialogModule,
+    MatSnackBarModule
+];
