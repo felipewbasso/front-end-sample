5587611621a5116f5c257afc57f558482f00e129
Felipe Basso <felipe.basso@techer.com.br>
2023-08-28 11:32:06 -0300

    app routes definition

---
+++ src/app/routes/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from "@angular/router";
+import { LayoutComponent } from "app/layout/layout.component";
+
+export const appRoutes: Routes = [
+
+    {
+        path: '',
+        // canMatch: [NoAuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {path: 'confirmation-required', loadChildren: () => import('app/modules/auth/confirmation-required/confirmation-required.module').then(m => m.AuthConfirmationRequiredModule)},
+            {path: 'forgot-password', loadChildren: () => import('app/modules/auth/forgot-password/forgot-password.module').then(m => m.AuthForgotPasswordModule)},
+            {path: 'reset-password', loadChildren: () => import('app/modules/auth/reset-password/reset-password.module').then(m => m.AuthResetPasswordModule)},
+            {path: 'sign-in', loadChildren: () => import('app/modules/auth/sign-in/sign-in.module').then(m => m.AuthSignInModule)}
+        ]
+    }
+
+];
