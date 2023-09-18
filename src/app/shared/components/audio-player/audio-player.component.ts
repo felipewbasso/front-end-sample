7deebf92c87153a97e06ab174d63d9b5ac51ba3c
Felipe Basso <felipe.basso@techer.com.br>
2023-09-18 17:19:01 -0300

    audio player layout

--- src/app/shared/components/audio-player/audio-player.component.ts
+++ src/app/shared/components/audio-player/audio-player.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from "@angular/core";
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
+import { ThemePalette } from "@angular/material/core";
 
-import { Subject } from "rxjs";
+import { Subject, fromEvent, takeUntil } from "rxjs";
+
 import { SharedModule } from "app/shared/shared.module";
 import { MaterialModule } from "app/shared/modules/material.module";
 import { SanitizerUrlPipe } from "app/shared/pipes/sanitizer-url/sanitizer-url.pipe";
+
+export type AudioPlayerComponentTheme = ThemePalette | 'basic';
 
 @Component({
     selector: 'app-audio-player',
@@ -19,7 +23,26 @@ import { SanitizerUrlPipe } from "app/shared/pipes/sanitizer-url/sanitizer-url.p
 })
 export class AudioPlayerComponent implements OnInit, OnDestroy {
 
-    url = 'assets/audio/believer-first-1-minute.mp3';
+    @Input('color') public set setColor(color: AudioPlayerComponentTheme) {
+        this.color = color;
+    }
+
+    @Input('url') public url: string;
+
+    /**
+     * Audio title
+     */
+    @Input('title') public title: string;
+
+    public color: AudioPlayerComponentTheme = 'primary';
+
+    public loading: boolean = true;
+
+    public player: HTMLAudioElement;
+
+    @ViewChild('audioPlayer', { static: true }) public set setAudioPlayerRef(ref: ElementRef<HTMLAudioElement>) {
+        this.player = ref.nativeElement;
+    };
 
     private readonly _unsubscribeAll = new Subject<void>();
 
@@ -30,10 +53,69 @@ export class AudioPlayerComponent implements OnInit, OnDestroy {
 
     ngOnInit(): void {
 
+        setTimeout(() => (this.loading = false), 15000);
+
+        fromEvent(this.player, 'playing')
+            .pipe(
+                takeUntil(this._unsubscribeAll)
+            )
+            .subscribe(() => {
+                // this.isPlaying = true;
+                // this.duration = Math.floor(this.player.duration);
+            });
+
+        fromEvent(this.player, 'pause')
+            .pipe(
+                takeUntil(this._unsubscribeAll)
+            )
+            .subscribe(() => {
+                // this.isPlaying = false;
+            });
+
+        fromEvent(this.player, 'timeupdate')
+            .pipe(
+                takeUntil(this._unsubscribeAll)
+            )
+            .subscribe(() => {
+                // this.progress = Math.round(this.player.currentTime * 20) / 20;
+                // this.currentTime = Math.floor(this.player.currentTime);
+            });
+
+        fromEvent(this.player, 'volume')
+            .pipe(
+                takeUntil(this._unsubscribeAll)
+            )
+            .subscribe(() => {
+                // this.volume = Math.floor(this.player.volume);
+            });
+
+        fromEvent(this.player, 'loadstart')
+            .pipe(
+                takeUntil(this._unsubscribeAll)
+            )
+            .subscribe(() => {
+                // this.loading = true;
+            });
+
+        fromEvent(this.player, 'loadeddata')
+            .pipe(
+                takeUntil(this._unsubscribeAll)
+            )
+            .subscribe(() => {
+                // this.loading = false;
+                // this.duration = Math.floor(this.player.duration);
+            });
     }
 
     ngOnDestroy(): void {
         this._unsubscribeAll.next();
         this._unsubscribeAll.complete();
+    }
+
+    /**
+     * Toggle volume action
+     */
+    public toggleVolume(): void {
+
     }
 }
