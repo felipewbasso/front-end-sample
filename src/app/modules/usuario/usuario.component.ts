603317f7050a6e05cf864443ec05234fc56b67e2
Felipe Basso <felipe.basso@techer.com.br>
2023-10-29 17:11:10 -0300

    usuario module - media watcher

--- src/app/modules/usuario/usuario.component.ts
+++ src/app/modules/usuario/usuario.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDrawerMode } from '@angular/material/sidenav';
+
+import { FuseMediaWatcherService } from '@fuse/services/media-watcher';
+
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'app-usuario',
     templateUrl: './usuario.component.html',
     styleUrls: [],
 })
-export class UsuarioComponent {
+export class UsuarioComponent implements OnInit, OnDestroy {
 
     public drawerMode: MatDrawerMode = 'side';
     public drawerOpened: boolean = true;
 
-    constructor() {}
+    private _unsubscribeAll = new Subject<void>();
+
+    constructor(
+        private _mediaWatcherService: FuseMediaWatcherService
+    ) {}
+
+    ngOnInit(): void {
+
+        this._mediaWatcherService.onMediaChange$
+            .pipe(
+                takeUntil(this._unsubscribeAll)
+            )
+            .subscribe(({ matchingAliases }) => {
+
+                if (matchingAliases.includes('lg')) {
+                    this.drawerMode = 'side';
+                    setTimeout(() => (this.drawerOpened = true));
+                } else {
+                    this.drawerMode = 'over';
+                    setTimeout(() => (this.drawerOpened = false));
+                }
+            });
+    }
+
+    ngOnDestroy(): void {
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
+    }
 }
